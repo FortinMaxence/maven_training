91ec9b03250ad7d2ea864e57441cf575ff725f3e
Maxence Fortin <fortin@et.esiea.fr>
2021-11-30 14:10:58 +0100

    Add test to match 100% coverage

--- src/test/java/fr.lernejo/SampleTest.java
+++ src/test/java/fr.lernejo/SampleTest.java
@@ -1,8 +1,15 @@
 package fr.lernejo;
 
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class SampleTest {
+
+    private final Sample sample = new Sample();
 
     @org.junit.jupiter.api.BeforeEach
     void setUp() {
@@ -14,9 +21,27 @@ class SampleTest {
 
     @org.junit.jupiter.api.Test
     void op() {
+
     }
 
-    @org.junit.jupiter.api.Test
-    void fact() {
+    @Test
+    void fact_neg() {
+        Assertions.assertThatExceptionOfType(IllegalArgumentException.class)
+            .isThrownBy(() -> sample.fact(-1))
+            .withMessage("N should be positive");
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+        "0, 1",
+        "1, 1",
+        "2, 2",
+        "3, 6",
+        "4, 24",
+        "13, 1932053504"
+    })
+    void fact_test_cases(int n, int expectedResult) {
+        int result = sample.fact(n);
+        Assertions.assertThat(result).isEqualTo(expectedResult);
     }
 }
